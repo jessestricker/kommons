5c74a4b6cbfad989e872ba4c89bd4c49cab1d4bb
Jesse Stricker <git@jessestricker.de>
2025-07-27 23:20:18 +0200

    chore: add lint job to checks workflow

--- kommons-hash/build.gradle.kts
+++ kommons-hash/build.gradle.kts
@@ -1,5 +1,9 @@
+import io.gitlab.arturbosch.detekt.Detekt
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+
+    alias(libs.plugins.detekt)
 }
 
 repositories {
@@ -20,4 +24,11 @@ kotlin {
         progressiveMode = true
         extraWarnings = true
     }
+}
+
+detekt {
+    source.setFrom(kotlin.sourceSets.map { it.kotlin })
+}
+tasks.withType<Detekt>{
+    jvmTarget = "1.8"
 }
