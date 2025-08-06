1af93bfe1c06afae3bd114d545158660267b9e54
Jesse Stricker <git@jessestricker.de>
2025-08-06 21:05:30 +0200

    chore: fix max-line-length warning

--- kommons-array/src/commonMain/kotlin/de/jessestricker/kommons/array/DelicateKommonsArrayApi.kt
+++ kommons-array/src/commonMain/kotlin/de/jessestricker/kommons/array/DelicateKommonsArrayApi.kt
@@ -11,7 +11,8 @@ package de.jessestricker.kommons.array
 @RequiresOptIn(
     message =
         "This is a delicate API and its use requires care. " +
-            "Make sure you fully read and understand documentation of the declaration that is marked as a delicate API.",
+            "Make sure you fully read and understand documentation " +
+            "of the declaration that is marked as a delicate API.",
     level = RequiresOptIn.Level.WARNING,
 )
 public annotation class DelicateKommonsArrayApi
